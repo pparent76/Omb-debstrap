99b3e1e5a7b33f695401de37b97b511201657fae
Pierre Parent <pierre.parent@pparent.fr>
2016-06-18 20:24:31 +0200

    Install missing packages for mailpile

--- required_packages.sh
+++ required_packages.sh
@@ -7,7 +7,8 @@ exit 1;
 fi
 apt-get install -y apt-transport-https
 apt-get remove -y resolvconf openresolv network-manager
-apt-get install -y curl libcurl4-openssl-dev build-essential postfix postfix-mysql postfix-pcre procmail apache2 mysql-server git gnupg openssh-server openssl python-virtualenv python-pip python-lxml python-dev libjpeg-dev tor torsocks dnsutils
+apt-get install -y curl libcurl4-openssl-dev build-essential postfix postfix-mysql postfix-pcre procmail apache2 mysql-server git gnupg openssh-server openssl python-virtualenv python-pip python-lxml python-dev libjpeg-dev tor torsocks dnsutils python-dev libxml2-dev libxslt1-dev zlib1g-dev python-jinja2 python-lxml python-pgpdump spambayes
+
 if [ "$?" -ne "0" ]; then
 exit 1;
 fi
