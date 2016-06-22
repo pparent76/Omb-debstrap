5ed3977440c95bce5d6e3734d85f0ce950bc919d
Pierre Parent <pierre.parent@pparent.fr>
2016-06-22 20:48:20 +0200

    Add specific revision when clonning a git repository, in order to have reproductible results.

--- install-repositories.sh
+++ install-repositories.sh
@@ -2,19 +2,37 @@
 
 cd /root/
 git clone https://github.com/pparent76/Omb-ihm.git
-cd Omb-ihm && make
+cd Omb-ihm
+# Get specific revisions in order to have reproductible results
+git reset --hard 557c9b40c1d3841fa8b93110fdc58fb66f136b2e
+if [ "$?" -ne "0" ]; then
+exit 1;
+fi
+make
 if [ "$?" -ne "0" ]; then
 exit 1;
 fi
 cd /root/
 git clone https://github.com/pparent76/Omb-cs-com.git
-cd Omb-cs-com/client/ && make && make install
+cd Omb-cs-com/client/ 
+# Get specific revisions in order to have reproductible results
+git reset --hard 664e581912222c9715bc2c9fc9219138527f5d0e
+if [ "$?" -ne "0" ]; then
+exit 1;
+fi
+make && make install
 if [ "$?" -ne "0" ]; then
 exit 1;
 fi
 cd /root/
 git clone https://github.com/pparent76/ttdnsd.git
-cd ttdnsd && make install
+cd ttdnsd
+# Get specific revisions in order to have reproductible results
+git reset --hard b9be844df3b6e061a37f00f431c65c0416091730
+if [ "$?" -ne "0" ]; then
+exit 1;
+fi
+make install
 if [ "$?" -ne "0" ]; then
 exit 1;
 fi
--- setup-mailpile.sh
+++ setup-mailpile.sh
@@ -6,6 +6,12 @@ mv Omb-Mailpile Mailpile
 # move into the newly created source repo
 cd /home/mailpile/Mailpile
 
+# Get specific revisions in order to have reproductible results
+git reset --hard f1d2c0f80c68b9c3a27cd1d4c986232a90a1a5d8
+if [ "$?" -ne "0" ]; then
+exit 1;
+fi
+
 # create a virtual environment directory
 virtualenv -p /usr/bin/python2.7 --system-site-packages mp-virtualenv
 if [ "$?" -ne "0" ]; then
