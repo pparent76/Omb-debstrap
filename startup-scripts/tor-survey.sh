7f204998776d65eab534c452c92b54a70eb60869
Pierre Parent <pierre.parent@pparent.fr>
2016-07-03 22:13:03 +0200

    Correct small bug

--- startup-scripts/tor-survey.sh
+++ startup-scripts/tor-survey.sh
@@ -6,7 +6,7 @@ while true; do
 
   hidden_service=$(cat /var/lib/tor/omb_hidden_service/hostname 2>/dev/null)
   
-  if [ ${#$hidden_service} -gt 15 ]; then
+  if [ ${#hidden_service} -gt 15 ]; then
     #Try to connect to our Own tor hidden service    
     torsocks wget  --tries=10  --timeout=45 http://$hidden_service/OK -O /tmp/wget-ok >/tmp/wget-tor-survey-res 2>&1
     ok=$(cat /tmp/wget-ok)
