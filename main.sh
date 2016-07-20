27c2fa88b45d9cd853701fe65b4ebc88e87771c4
Armando Lüscher <armando@noplanman.ch>
2016-07-20 12:49:28 +0200

    Improve and rename required-packages.sh Prettify main.sh a bit more

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 set -x
 
 ./cleanup.sh
-./required_packages.sh
+./required-packages.sh
 if [ "$?" -ne "0" ]; then
   echo "Error while installing required debian packages."
   echo "Please check your apt-get config."
@@ -61,8 +61,8 @@ fi
 
 pip install -r /home/mailpile/Mailpile/requirements.txt
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up mailpile.";
-exit 1;
+  echo "Error while setting up mailpile."
+  exit 1
 fi
 
 mkdir /etc/omb/
@@ -79,27 +79,27 @@ fi
 
 ./setup-iptables.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up iptables.";
-exit 1;
+  echo "Error while setting up iptables."
+  exit 1
 fi
 
-#Temporarilly use google dns server and flush iptables
-#to make the upgrade will be overwritten at first startup
-echo "nameserver 8.8.8.8">/etc/resolv.conf
+# Temporarily use google dns server and flush iptables
+# to make sure the upgrade will be overwritten at first startup
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 iptables -F
 ip6tables -F
 apt-get upgrade -y
 
-#Make sure to fetch time every hour in the crontab
-(crontab -l 2>/dev/null; echo "00 * * * * service ntp stop ; torsocks ntpdate -s ntp.univ-lyon1.fr; service ntp start") | crontab -
+# Make sure to fetch time every hour in the crontab
+(crontab -l 2>/dev/null; echo "00 * * * * service ntp stop; torsocks ntpdate -s ntp.univ-lyon1.fr; service ntp start") | crontab -
 
-#Make sure to sync every minute
+# Make sure to sync every minute
 (crontab -l 2>/dev/null; echo "* * * * * sync") | crontab -
 
 if [ "$DOCKER" != "yes" ]; then
   echo "Rebooting in 5 seconds..."
-  sleep 5;
+  sleep 5
   reboot
-else
-  exit 0;
 fi
+
+exit 0
--- required-packages.sh
+++ required-packages.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
-grep "jessie-backports" /etc/apt/sources.list
-if [ "$?" -ne "0" ]; then
-echo "deb http://ftp.debian.org/debian jessie-backports main">>/etc/apt/sources.list
-fi
-grep "security.debian.org" /etc/apt/sources.list
-if [ "$?" -ne "0" ]; then
-echo "deb http://security.debian.org/ jessie/updates main">>/etc/apt/sources.list
+
+set -e
+
+if ! grep "jessie-backports" /etc/apt/sources.list; then
+  echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 fi
 
-#Fetch official debian key for Raspbian
+if ! grep "security.debian.org" /etc/apt/sources.list; then
+  echo "deb http://security.debian.org/ jessie/updates main" >> /etc/apt/sources.list
+fi
+
+# Fetch official debian keys for Raspbian
 cp trusted.gpg.d/* /etc/apt/trusted.gpg.d/
 
 apt-get update
 
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
-
 apt-get install -y apt-transport-https
 apt-get remove -y resolvconf openresolv network-manager
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl ntpdate sudo rsyslog iptables iptables-persistent libcurl4-openssl-dev wget build-essential postfix postfix-mysql postfix-pcre procmail apache2 mysql-server git gnupg openssh-server openssl python-virtualenv python-pip python-lxml python-dev libjpeg-dev tor torsocks dnsutils python-dev libxml2-dev libxslt1-dev zlib1g-dev python-jinja2 python-lxml python-pgpdump spambayes
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
 
 apt-get install -y certbot -t jessie-backports
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
 
-service mysql start;
+service mysql start
 
-exit 0;
+exit 0
