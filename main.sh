f80d751ca3f2a4a11caa95f3b48493b70e5194d5
Armando Lüscher <armando@noplanman.ch>
2016-07-20 12:37:40 +0200

    Prettify main.sh

--- main.sh
+++ main.sh
@@ -5,51 +5,58 @@ set -x
 ./cleanup.sh
 ./required_packages.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while installing required debian packages";
-echo "Please check your apt-get config";
-exit 1;
+  echo "Error while installing required debian packages."
+  echo "Please check your apt-get config."
+  exit 1
 fi
+
 cd Local_postfix_conf && make install && cd ..
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up postfix";
-exit 1;
+  echo "Error while setting up postfix."
+  exit 1
 fi
+
 ./install-repositories.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while installing Own-Mailbox git repositories.";
-exit 1;
+  echo "Error while installing Own-Mailbox git repositories."
+  exit 1
 fi
+
 ./setup-apache.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up apache.";
-exit 1;
+  echo "Error while setting up apache."
+  exit 1
 fi
+
 ./make-users.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up users.";
-exit 1;
+  echo "Error while setting up users."
+  exit 1
 fi
+
 ./setup-startup.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up startup.";
-exit 1;
+  echo "Error while setting up startup."
+  exit 1
 fi
+
 ./setup-rng-tool.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up rng-tools.";
-exit 1;
+  echo "Error while setting up rng-tools."
+  exit 1
 fi
+
 ./setup-hostname.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up hostname.";
-exit 1;
+  echo "Error while setting up hostname."
+  exit 1
 fi
 
 pkill python2
 su mailpile -c ./setup-mailpile.sh
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up mailpile.";
-exit 1;
+  echo "Error while setting up mailpile."
+  exit 1
 fi
 
 pip install -r /home/mailpile/Mailpile/requirements.txt
@@ -59,15 +66,15 @@ exit 1;
 fi
 
 mkdir /etc/omb/
-echo "nameserver 127.0.0.1">/etc/resolv.conf.head
+echo "nameserver 127.0.0.1" > /etc/resolv.conf.head
 
 cp torrc /etc/tor/torrc
 touch /var/log/tor.log
 chown tor /var/log/tor.log
-echo "AllowInbound 1" >> /etc/tor/torsocks.conf 
+echo "AllowInbound 1" >> /etc/tor/torsocks.conf
 if [ "$?" -ne "0" ]; then
-echo "Error while setting up torsocks.";
-exit 1;
+  echo "Error while setting up torsocks."
+  exit 1
 fi
 
 ./setup-iptables.sh
