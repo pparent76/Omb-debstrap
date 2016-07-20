8aac1450bbbb07abfb5544c1ada9a99d855c9c67
Armando Lüscher <armando@noplanman.ch>
2016-07-20 13:09:39 +0200

    Rewrite and clean up multiple scripts.

--- startup-scripts/ttdnsd-survey.sh
+++ startup-scripts/ttdnsd-survey.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-
 while true; do
-  dig own-mailbox.com
-  res=$?
-  
-  #retry to be sure
-  if [ "$res" -ne "0" ]; then
-  dig own-mailbox.com
-  res=$?
-  fi
-  
-  #retry to be sure
-  if [ "$res" -ne "0" ]; then
-  dig own-mailbox.com
-  res=$?
-  fi
-  
-  #if we can't resolve own-mailbox.com restart ttdnsd and reconfigure resolv.conf
-  if [ "$res" -ne "0" ]; then
-  killall ttdnsd
-  echo "nameserver 127.0.0.1">/etc/resolv.conf;
-  ttdnsd -l
+
+  # Try 3 times to resolve own-mailbox.com
+  dig own-mailbox.com +tries=3
+
+  # If we can't resolve own-mailbox.com restart ttdnsd and reconfigure resolv.conf
+  if [ "$?" -ne "0" ]; then
+    killall ttdnsd
+    echo "nameserver 127.0.0.1" > /etc/resolv.conf
+    ttdnsd -l
   fi
 
-  sleep 60;
+  sleep 60
 
 done
