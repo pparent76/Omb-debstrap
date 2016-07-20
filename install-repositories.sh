793c292049b9eeea2e4618f39669f0b5c09efc7d
Armando Lüscher <armando@noplanman.ch>
2016-07-20 13:11:51 +0200

    Improve and simplify error handling.

--- install-repositories.sh
+++ install-repositories.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
+set -e
+
 cd /root/
 git clone https://github.com/pparent76/Omb-ihm.git
 cd Omb-ihm
-# Get specific revisions in order to have reproductible results
+# Get specific revision in order to have reproducible results
 git reset --hard 16e49b29df2027b99eecb3138669d09b8ebe8f50
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
 make
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
+
 cd /root/
 git clone https://github.com/pparent76/Omb-cs-com.git
-cd Omb-cs-com/client/ 
-# Get specific revisions in order to have reproductible results
+cd Omb-cs-com/client/
+# Get specific revision in order to have reproducible results
 git reset --hard 664e581912222c9715bc2c9fc9219138527f5d0e
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
 make && make install
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
+
 cd /root/
 git clone https://github.com/pparent76/ttdnsd.git
 cd ttdnsd
-# Get specific revisions in order to have reproductible results
+# Get specific revision in order to have reproducible results
 git reset --hard b9be844df3b6e061a37f00f431c65c0416091730
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
 make install
-if [ "$?" -ne "0" ]; then
-exit 1;
-fi
 
-exit 0;
+exit 0
