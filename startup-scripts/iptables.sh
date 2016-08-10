50657f76d17a9a168afdac8cf5d0b42253c693ba
Pierre Parent <pparent@comminter.com>
2016-08-10 09:56:23 +0200

    Allowing cstate related connections for input

--- startup-scripts/iptables.sh
+++ startup-scripts/iptables.sh
@@ -6,14 +6,11 @@ iptables -A INPUT -s 192.168.0.0/16 -j ACCEPT
 iptables -A INPUT -s 10.0.0.0/8     -j ACCEPT
 iptables -A INPUT -s 172.16.0.0/12  -j ACCEPT
 
-# For other than local network block all ports that we have opened
-iptables -A INPUT -p tcp --destination-port 80  -j DROP
-iptables -A INPUT -p tcp --destination-port 443 -j DROP
-iptables -A INPUT -p tcp --destination-port 25  -j DROP
-iptables -A INPUT -p tcp --destination-port 22  -j DROP
-iptables -A INPUT -p tcp --destination-port 111 -j DROP
-iptables -A INPUT -p tcp --destination-port 993 -j DROP
-iptables -A INPUT -p tcp --destination-port 143 -j DROP
+# Allow established or related connections
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
+
+# Drop everyhting else
+iptables -A INPUT -j DROP
 
 # Drop DNS outgoing connections
 iptables -A OUTPUT -d 192.168.0.0/16 -p udp --destination-port 53 -j DROP
@@ -32,14 +29,20 @@ iptables -A OUTPUT -d 172.16.0.0/12  -j ACCEPT
 # Drop all other outgoing connections
 iptables -A OUTPUT -j DROP
 
+################################################################
+#			IPV6
+################################################################
+
+#Drop all outgoing connection except for ::1
 ip6tables -A OUTPUT -d ::1 -j ACCEPT
 ip6tables -A OUTPUT -j DROP
 
+# Allow connections incoming for ::1
 ip6tables -A INPUT -s ::1 -d ::1 -j ACCEPT
-ip6tables -A INPUT -p tcp --destination-port 80  -j DROP
-ip6tables -A INPUT -p tcp --destination-port 443 -j DROP
-ip6tables -A INPUT -p tcp --destination-port 25  -j DROP
-ip6tables -A INPUT -p tcp --destination-port 22  -j DROP
-ip6tables -A INPUT -p tcp --destination-port 111 -j DROP
-ip6tables -A INPUT -p tcp --destination-port 993 -j DROP
-ip6tables -A INPUT -p tcp --destination-port 143 -j DROP
+
+# Allow established or related connections
+ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
+
+# Drop everyhting else
+ip6tables -A INPUT -j DROP
+
