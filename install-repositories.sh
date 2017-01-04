76121ba021b19eb2398223686a92ebfd749d9bdc
Dashamir Hoxha <dashohoxha@gmail.com>
2017-01-04 16:18:05 +0100

    Fix getting repositories.

--- install-repositories.sh
+++ install-repositories.sh
@@ -3,24 +3,21 @@
 set -e
 
 cd /root/
-git clone https://github.com/Own-Mailbox/ihm.git
-cd Omb-ihm
-# Get specific revision in order to have reproducible results
- git reset --hard e2943286bbb10525ebfaf203f9d3e2b5ad7a154b
+rm -rf ihm/
+git clone --branch alpha https://github.com/Own-Mailbox/ihm
+cd ihm/
 make
 
 cd /root/
-git clone https://github.com/Own-Mailbox/cs-com.git
-cd Omb-cs-com/client/
-# Get specific revision in order to have reproducible results
-git reset --hard 664e581912222c9715bc2c9fc9219138527f5d0e
+rm -rf cs-com/
+git clone --branch alpha https://github.com/Own-Mailbox/cs-com
+cd cs-com/client/
 make && make install
 
 cd /root/
-git clone https://github.com/Own-Mailbox/ttdnsd.git
-cd ttdnsd
-# Get specific revision in order to have reproducible results
-git reset --hard b9be844df3b6e061a37f00f431c65c0416091730
+rm -rf ttdnsd/
+git clone --branch alpha https://github.com/Own-Mailbox/ttdnsd
+cd ttdnsd/
 make install
 
 exit 0
