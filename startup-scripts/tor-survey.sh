ad716c6c8b88fba342b724c4b842df283341209e
Pierre Parent <pierre.parent@pparent.fr>
2017-09-30 11:39:15 +0200

    Revert "One more improvement on tor-survey"
    This reverts commit 575e5d32cc0e8a7d222cf270ed6b0ebb0caf9a96.

--- startup-scripts/tor-survey.sh
+++ startup-scripts/tor-survey.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-sleep 100
+sleep 300
 
 while true; do
 
   # Try to connect to our own tor hidden service
   host=$(cat /var/lib/tor/omb_hidden_service/hostname 2>/dev/null)
 
-  #if we allready have a tor hostname
-  if [ ${#host} -gt 14 ]; then
-    torsocks wget --tries=10 --timeout=45 http://$host/OK -O /tmp/wget-ok > /tmp/wget-tor-survey-res 2>&1
-    ok=$(cat /tmp/wget-ok)
-
-    # If we could not connect properly to the service, restart tor
-    if [ "$ok" != "OK" ]; then
-        killall tor
-        su tor -c tor&
-    fi
-
-    sleep 400
-  else
-    sleep 10
+  if [ ${#host} -le 15 ]; then
+    # Try to connect to the proxy server
+    host=proxy.omb.one
   fi
-  
+
+  torsocks wget --tries=10 --timeout=45 http://$host/OK -O /tmp/wget-ok > /tmp/wget-tor-survey-res 2>&1
+  ok=$(cat /tmp/wget-ok)
+
+  # If we could not connect properly to the service, restart tor
+  if [ "$ok" != "OK" ]; then
+    killall tor
+    su tor -c tor&
+  fi
+
+  sleep 400
+
 done
