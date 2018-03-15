4a808764066649c0be32897f301d3d54f08757bd
Pierre Parent <pierre.parent@pparent.fr>
2018-03-15 12:52:41 +0100

    Go back to trunk repositories.

--- setup-scripts/install-repositories.sh
+++ setup-scripts/install-repositories.sh
@@ -9,20 +9,20 @@ set -e
 cp config.sh /root/
 cd /root/
 rm -rf ihm/
-git clone --branch alpha https://github.com/Own-Mailbox/ihm
+git clone https://github.com/Own-Mailbox/ihm
 cp config.sh ihm/omb-config.sh
 cd ihm/
 make
 
 cd /root/
 rm -rf cs-com/
-git clone --branch alpha https://github.com/Own-Mailbox/cs-com
+git clone https://github.com/Own-Mailbox/cs-com
 cd cs-com/client/
 make && make install
 
 cd /root/
 rm -rf ttdnsd/
-git clone --branch alpha https://github.com/Own-Mailbox/ttdnsd
+git clone https://github.com/Own-Mailbox/ttdnsd
 cd ttdnsd/
 make install
 
